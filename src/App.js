052b235daae6758320eeed741bcd00b8ca0eb6e7
murali590 <murali427427@gmail.com>
2020-03-09 18:28:32 +0530

    sigin and register form added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Signin from  './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
+import Register from './components/Register.js/Register';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -18,36 +20,65 @@ class App extends Component {
 		super();
 		this.state={
 			input: '',
-			imageUrl: ''
+			imageUrl: '',
+			box: {},
+			route: 'Signin'
 		}
 	} 
+	calculateFaceLocation = (data)=>{
+		const clarifaiFace=data.outputs[0].data.regions[0].region_info.bounding_box;
+        const  image=document.getElementById('inputimage');
+        const width=Number(image.width);
+        const height=Number(image.height);
+        return {
+        	leftCol: clarifaiFace.left_col*width,
+        	topRow: clarifaiFace.top_row*height,
+        	rightCol: width -(clarifaiFace.right_col * width),
+        	bottomRow: height -(clarifaiFace.bottom_row * height)
+        }
+	}
+
+	displayFaceBox =(box)=>{
+		console.log(box)
+		this.setState({box: box})
+	}
+
+	onRouteChange =(route)=>{
+		this.setState({route: route })
+	}
+
+
 	onInputChange =(event)=>{ 
               this.setState({input: event.target.value});
 	 }
-	 onButtonSubmit =()=>{
+	onButtonSubmit =()=>{
 	 	this.setState({imageUrl: this.state.input})
 	 	app.models.predict(
-	 		Clarifai.FACE_DETECT_MODEL, 
-	 		this.state.input).then(
-            function(response) {
-             console.log(response);
-            },
-            function(err) {
-              console.log(err);
-           }
-  );
-	 }
+	 		Clarifai.FACE_DETECT_MODEL, this.state.input)
+	 	.then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err))  
+         }
+
  render(){ 
      return (
     <div className="App">
-     <Navigation />
-     <Logo />
-     <Rank />
-     <ImageLinkForm  
-     onInputChange={this.onInputChange}
-     onButtonSubmit={this.onButtonSubmit}
-     />
-     <FaceRecognition imageUrl={this.state.imageUrl} />
+     <Navigation onRouteChange={this.onRouteChange}/>
+     { this.state.route === 'home'?
+       <div><Logo />
+              <Rank />
+              <ImageLinkForm  
+                  onInputChange={this.onInputChange}
+                  onButtonSubmit={this.onButtonSubmit}
+               />
+               <FaceRecognition 
+               imageUrl={this.state.imageUrl} 
+               box={this.state.box}/>
+         </div>
+           :   (
+           this.state.route === 'Signin')  ?
+           <Signin onRouteChange={this.onRouteChange}/>
+           :<Register onRouteChange={this.onRouteChange}/>
+      }
     </div>
   );
 }
